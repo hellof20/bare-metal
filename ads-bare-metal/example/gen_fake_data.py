86905f24e3cbe508cd3fa369ea38911a9aff20fd
Wenming Pan <pwm@google.com>
2022-07-12 14:58:08 +0800

    Tue Jul 12 14:58:08 CST 2022

--- ads-bare-metal/example/gen_fake_data.py
+++
@@ -1,3 +0,0 @@
-print('customer_id,conversion_action_id,gclid,conversion_date_time,conversion_value,currency')
-for i in range(123):
-  print(f'1234567890,123{i},Cj0KCQjwppSEBhCGARIsANIs4p47NN6uY5sI7bMw8bzA9E-hemYXU7PU34CSMoSIt_i4-YH3piaOVHIaAiTuEALw_{i},2021-04-26 06:10+08:00,0.1,USD')
--- baremetal.py
+++ baremetal.py
@@ -3,8 +3,11 @@ import os
 from flask import Flask
 from flask import request,render_template
 from flask import jsonify
+from python_terraform import *
 
 app = Flask(__name__)
+
+bm = Terraform(working_dir='tf-tutorial')
 
 @app.route('/health')
 def health():
@@ -14,15 +17,46 @@ def health():
 def index():
     return render_template('baremetal.html')
 
-@app.route('/install', methods=['POST'])
-def process_register():
+@app.route('/apply', methods=['POST'])
+def apply():
     PROJECT_ID = request.form.get("project_id")
     BUCKET_NAME = request.form.get("bucket_name")
-    print(PROJECT_ID)
-    print(BUCKET_NAME)
-    os.system("mkdir -p customer_" + PROJECT_ID)
-    os.system("cd ads-bare-metal && PROJECT_ID=%s BUCKET_NAME=%s nohup bash bin/deploy.sh > ../customer_%s/install.log 2>&1 &"%(PROJECT_ID, BUCKET_NAME, PROJECT_ID))
-    return "installing"
+    GCP_PROJECT = request.form.get("gcp_project")
+    if GCP_PROJECT == "BareMetal":
+        bm.init()
+        return_code, stdout, stderr = bm.apply(skip_plan=True, var={'project':PROJECT_ID})
+        if return_code == 0:
+            print(stdout)
+            return 'Deploy success'
+        else:
+            print(stdout)
+            print(stderr)
+            return 'Deploy failed'        
+    else:
+        return 'not supported'
+
+
+@app.route('/destroy', methods=['POST'])
+def destroy():
+    PROJECT_ID = request.form.get("project_id")
+    BUCKET_NAME = request.form.get("bucket_name")
+    GCP_PROJECT = request.form.get("gcp_project")
+    if GCP_PROJECT == "BareMetal":
+        return_code, stdout, stderr = bm.destroy(force=IsNotFlagged, auto_approve=True, var={'project':PROJECT_ID})
+        if return_code == 0:
+            print(stdout)
+            return 'Destroy success'
+        else:
+            print(stdout)        
+            print(stderr)
+            return 'Destroy failed'
+    else:
+        return 'not supported'   
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=80)
+
+
+    #os.system("mkdir -p customer_" + PROJECT_ID)
+    #os.system("cd ads-bare-metal && PROJECT_ID=%s BUCKET_NAME=%s nohup bash bin/deploy.sh > ../customer_%s/install.log 2>&1 &"%(PROJECT_ID, BUCKET_NAME, PROJECT_ID))
+    #os.system("cd tf-tutorial && terraform apply -auto-approve")
